46e037ffb251a5ed7fb4be5f2bd68bab46caea9d
DESKTOP-JNL15CL\Andy <767046365@qq.com>
2017-01-14 00:31:54 +0800

    完成api封装

--- routes/restfulAPi.js
+++ routes/restfulAPi.js
@@ -2,10 +2,10 @@
 var express=require('express');
 var router=express.Router();
 
-router.post('/rank/:email', function (req, res) {
+router.get('/rank=:email', function (req, res) {
     var email=req.params.email;
     //console.log("email:",email);
-    var selectSql="select account_name,credit,tel from account where email="+"'"+email+"'";
+    var selectSql="select account_name,credit,email from account where email="+"'"+escape(email)+"'";
     globalConnection.query(selectSql,function(err,result,fields){
         if(err){
             console.log('get rank err:' + err) ;
@@ -19,6 +19,27 @@ router.post('/rank/:email', function (req, res) {
         else
         {
             res.send('no such a email');
+        }
+    }) ;
+});
+
+router.get('/ranklist=:credit', function (req, res) {
+    var credit=req.params.credit;
+    //console.log("email:",email);
+    var selectSql="select account_name,credit,email from account where credit >="+"'"+escape(credit)+"'";
+    globalConnection.query(selectSql,function(err,result,fields){
+        if(err){
+            console.log('get ranklist err:' + err) ;
+            return ;
+        }
+        //console.log(result);
+        if(result[0])
+        {
+            res.send(result);
+        }
+        else
+        {
+            res.send('no conform to the standard');
         }
     }) ;
 });
